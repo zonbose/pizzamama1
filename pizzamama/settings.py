5027c1f7d3e6a027dc2817bd4195a892636a7194
Zon Bose <zabes141@gmail.com>
2021-10-10 18:48:26 +0530

    settings change

--- pizzamama/settings.py
+++ pizzamama/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 django_heroku.settings(locals())
